c605f231f2a988ec906f5e5edff593abb8ef298d
NE96 <ne58@njit.edu>
2018-05-05 17:06:29 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AddTodo from './components/addTodo/';
+
+const App = () => (
+    <div>
+        My App
+        <h1>Todo list</h1>
+        <AddTodo submitTodo={() => {}} />
+    </div>
+);
+
+export default App;
