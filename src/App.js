874b1dbc55a0d0114aea4d536071b4e0b6c20732
NE96 <ne58@njit.edu>
2018-05-07 18:28:28 -0400

    fourth commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AddTodo from './components/addTodo/';
+import TodoList from './components/todoList';
 import actions from './actions/';
 
-export const App = ({ submitTodo }) => (
+export const App = ({ submitTodo, todos, deleteTodo }) => (
     <div>
         <h1>Todo list</h1>
         <AddTodo submitTodo={submitTodo} />
+        <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
 );
 
 App.propTypes = {
     submitTodo: PropTypes.func.isRequired,
+    todos: PropTypes.arrayOf(PropTypes.shape(
+        {
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+        },
+    )).isRequired,
+    deleteTodo: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => state.todoListApp;
@@ -22,6 +31,10 @@ const mapDispatchToProps = dispatch => ({
         if (text) {
             dispatch(actions.submitTodo(text));
         }
+    },
+
+    deleteTodo: (id) => {
+        dispatch(actions.deleteTodo(id));
     },
 });
 
