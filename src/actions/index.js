e836fcb7de53ba78dcf2b770f1720d649317f5d7
NE96 <ne58@njit.edu>
2018-05-08 01:09:12 -0400

    fifth commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,6 +15,19 @@ const actions = {
             text,
         };
     },
+
+    deleteTodo(id) {
+        return {
+            type: types.DELETE_TODO,
+            id,
+        };
+    },
+
+    undeleteTodo() {
+        return {
+            type: types.UNDELETE_TODO,
+        };
+    },
 };
 
 export default actions;
